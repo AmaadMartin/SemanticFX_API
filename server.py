299ff90210b6a098eafcf57aa5c444fc15d09aa7
Wadih Pazos <wadihjr@wpazos.com>
2023-12-29 12:57:57 -0500

    created api

--- server.py
+++ server.py
@@ -2,14 +2,14 @@ from flask import Flask, request, jsonify
 from EffectGenerator import GPT  # Import your GPT class from your script
 
 app = Flask(__name__)
-gpt = GPT()
 
-@app.route('/engineer_prompt', methods=['POST'])
+@app.route('/get-params', methods=['POST'])
 def engineer_prompt():
     data = request.json
     query = data['query']
-    response = gpt.engineerPrompt(query)
-    return jsonify(response)
+    gpt = GPT()
+    gpt.engineerPrompt(query)
+    return jsonify(gpt.parameters)
 
 if __name__ == '__main__':
     app.run()
